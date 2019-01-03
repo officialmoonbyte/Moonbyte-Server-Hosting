f3b301c4801f80392598973c2fe69c3cb8142bf9
corporate@moonbyte.net <corporate@moonbyte.net>
2019-01-03 14:46:02 -0600

    l

---
+++ Moonbyte Server Hosting/Moonbyte Server Hosting/MainForm/mainForm.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Moonbyte.Client.ServerHosting.Main
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.materialTabControl1 = new IndieGoat.MaterialFramework.Controls.MaterialTabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.materialTabPage1 = new IndieGoat.MaterialFramework.Controls.MaterialTabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.materialTabPage2 = new IndieGoat.MaterialFramework.Controls.MaterialTabPage();
+            this.tabHeader1 = new IndieGoat.MaterialFramework.Controls.TabHeader();
+            this.materialTabPage3 = new IndieGoat.MaterialFramework.Controls.MaterialTabPage();
+            this.materialTabPage4 = new IndieGoat.MaterialFramework.Controls.MaterialTabPage();
+            this.materialTabPage5 = new IndieGoat.MaterialFramework.Controls.MaterialTabPage();
+            this.materialTabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // materialTabControl1
+            // 
+            this.materialTabControl1.Controls.Add(this.materialTabPage3);
+            this.materialTabControl1.Controls.Add(this.materialTabPage4);
+            this.materialTabControl1.Controls.Add(this.materialTabPage5);
+            this.materialTabControl1.Location = new System.Drawing.Point(1, 65);
+            this.materialTabControl1.Name = "materialTabControl1";
+            this.materialTabControl1.SelectedIndex = 0;
+            this.materialTabControl1.Size = new System.Drawing.Size(798, 372);
+            this.materialTabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(192, 74);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // materialTabPage1
+            // 
+            this.materialTabPage1.BackColor = System.Drawing.Color.Transparent;
+            this.materialTabPage1.Location = new System.Drawing.Point(4, 22);
+            this.materialTabPage1.Name = "materialTabPage1";
+            this.materialTabPage1.Size = new System.Drawing.Size(790, 383);
+            this.materialTabPage1.TabIndex = 0;
+            this.materialTabPage1.Text = "tabPage1";
+            this.materialTabPage1.Visible = false;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // materialTabPage2
+            // 
+            this.materialTabPage2.BackColor = System.Drawing.Color.Transparent;
+            this.materialTabPage2.Location = new System.Drawing.Point(4, 22);
+            this.materialTabPage2.Name = "materialTabPage2";
+            this.materialTabPage2.Size = new System.Drawing.Size(790, 383);
+            this.materialTabPage2.TabIndex = 1;
+            this.materialTabPage2.Text = "tabPage2";
+            this.materialTabPage2.Visible = false;
+            // 
+            // tabHeader1
+            // 
+            this.tabHeader1.AddButtonBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(57)))), ((int)(((byte)(84)))));
+            this.tabHeader1.AddButtonHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(120)))), ((int)(((byte)(120)))));
+            this.tabHeader1.AllowDrop = true;
+            this.tabHeader1.BasedTabControl = this.materialTabControl1;
+            this.tabHeader1.CloseButtonHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.tabHeader1.EnableAddButton = false;
+            this.tabHeader1.FontColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.tabHeader1.Hover_BackColor = System.Drawing.Color.White;
+            this.tabHeader1.Hover_BorderColor = System.Drawing.Color.White;
+            this.tabHeader1.Hover_FontColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.tabHeader1.Hover_TopBarColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(171)))), ((int)(((byte)(182)))));
+            this.tabHeader1.Location = new System.Drawing.Point(51, 33);
+            this.tabHeader1.Name = "tabHeader1";
+            this.tabHeader1.ScrollInt = 0;
+            this.tabHeader1.Selected_BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(249)))), ((int)(((byte)(250)))));
+            this.tabHeader1.Selected_BorderColor = System.Drawing.Color.White;
+            this.tabHeader1.Selected_FontColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
+            this.tabHeader1.Selected_TopBarColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(132)))), ((int)(((byte)(255)))));
+            this.tabHeader1.ShowCloseButton = false;
+            this.tabHeader1.Size = new System.Drawing.Size(691, 32);
+            this.tabHeader1.TabBackColor = System.Drawing.Color.White;
+            this.tabHeader1.TabBorderColor = System.Drawing.Color.White;
+            this.tabHeader1.TabIndex = 3;
+            this.tabHeader1.TopBarColor = System.Drawing.Color.White;
+            // 
+            // materialTabPage3
+            // 
+            this.materialTabPage3.Location = new System.Drawing.Point(4, 22);
+            this.materialTabPage3.Name = "materialTabPage3";
+            this.materialTabPage3.Size = new System.Drawing.Size(790, 346);
+            this.materialTabPage3.TabIndex = 0;
+            this.materialTabPage3.Text = "materialTabPage3";
+            // 
+            // materialTabPage4
+            // 
+            this.materialTabPage4.Location = new System.Drawing.Point(4, 22);
+            this.materialTabPage4.Name = "materialTabPage4";
+            this.materialTabPage4.Size = new System.Drawing.Size(790, 346);
+            this.materialTabPage4.TabIndex = 1;
+            this.materialTabPage4.Text = "materialTabPage4";
+            // 
+            // materialTabPage5
+            // 
+            this.materialTabPage5.Location = new System.Drawing.Point(4, 22);
+            this.materialTabPage5.Name = "materialTabPage5";
+            this.materialTabPage5.Size = new System.Drawing.Size(790, 346);
+            this.materialTabPage5.TabIndex = 2;
+            this.materialTabPage5.Text = "materialTabPage5";
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(127)))), ((int)(((byte)(215)))));
+            this.BorderSize = 2;
+            this.ClientSize = new System.Drawing.Size(800, 438);
+            this.Controls.Add(this.tabHeader1);
+            this.Controls.Add(this.materialTabControl1);
+            this.HeaderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(127)))), ((int)(((byte)(215)))));
+            this.HeaderHeight = 1;
+            this.Name = "mainForm";
+            this.Showicon = false;
+            this.Load += new System.EventHandler(this.mainForm_Load);
+            this.materialTabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private IndieGoat.MaterialFramework.Controls.MaterialTabControl materialTabControl1;
+        private IndieGoat.MaterialFramework.Controls.MaterialTabPage materialTabPage1;
+        private IndieGoat.MaterialFramework.Controls.MaterialTabPage materialTabPage2;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private IndieGoat.MaterialFramework.Controls.TabHeader tabHeader1;
+        private IndieGoat.MaterialFramework.Controls.MaterialTabPage materialTabPage3;
+        private IndieGoat.MaterialFramework.Controls.MaterialTabPage materialTabPage4;
+        private IndieGoat.MaterialFramework.Controls.MaterialTabPage materialTabPage5;
+    }
+}
---
+++ Moonbyte Server Hosting/Moonbyte Server Hosting/MainForm/mainForm.cs
@@ -0,0 +1,19 @@
+﻿using IndieGoat.MaterialFramework.Controls;
+using System;
+
+namespace Moonbyte.Client.ServerHosting.Main
+{
+    public partial class mainForm : MaterialForm
+    {
+
+        #region Startup
+
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void mainForm_Load(object sender, EventArgs e) { }
+        #endregion
+    }
+}
